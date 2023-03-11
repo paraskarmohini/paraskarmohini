c48ea07b557da4c29cbf782d444da282f974025b
paraskarmohini <105348188+paraskarmohini@users.noreply.github.com>
2023-03-11 04:12:48 -0800

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void calculator(int a,int b)
+{
+printf("\n addition  of two numbers is: %d",a+b);
+printf("\n subtraction of two numbers is: %d",a-b);
+printf("\n multiplicatio of two numbers is:%d",a*b);
+printf("\n division of two numbers is: %d",a/b);
+}
+
