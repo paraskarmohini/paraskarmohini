fe712fc2dc2289b99658442a0d03847157266566
paraskarmohini <105348188+paraskarmohini@users.noreply.github.com>
2023-03-23 02:53:35 -0700

    Create insert_records.sql

---
+++ insert_records.sql
@@ -0,0 +1,3 @@
+INSERT INTO students VALUES(1,"nikita",20,"A"),(2,"akanksha",22,"C"),(3,"nikhil",17,"B"),
+(4,"akita",18,"C"),(5,"vikas",24,"A"),(6,"amruta",18,"B"),(7,"ruchita",20,"B"),(8,"ganesh",21,"A"),
+(9,"asmita",16,"A"),(10,"priyanka",20,"C");
