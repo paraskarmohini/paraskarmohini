a9871022a22c4898cd663d5cb125ebd9bec75725
paraskarmohini <105348188+paraskarmohini@users.noreply.github.com>
2023-03-23 02:51:36 -0700

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,5 @@
+/*create table named "students"with following structure:id,name,age,grade*/
+
+CREATE database INFO;
+USE INFO;
+CREATE TABLE students(id int,name varchar(100),age int,grade varchar(50));
